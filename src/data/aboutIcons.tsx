19b3b55fd9e89f0111b84386dc5d3b017803ee4f
terrencejihoonjung <terrencejung928@gmail.com>
2023-12-18 02:15:55 -0800

    changed icon imports

--- src/data/aboutIcons.tsx
+++ src/data/aboutIcons.tsx
@@ -1,25 +1,47 @@
+import Java from "../../../assets/Java.png";
+import HTML from "../../../assets/HTML.png";
+import CSS from "../../../assets/CSS.png";
+import Tailwind from "../../../assets/Tailwind.png";
+import JavaScript from "../../../assets/JavaScript.png";
+import TypeScript from "../../../assets/TypeScript.png";
+import React from "../../../assets/React.png";
+import Node from "../../../assets/Node.png";
+import Express from "../../../assets/Express.png";
+import Postgres from "../../../assets/Postgres.png";
+import Figma from "../../../assets/Figma.png";
+import Git from "../../../assets/Git.png";
+import Vercel from "../../../assets/Vercel.png";
+import Postman from "../../../assets/Postman.png";
+import VSCode from "../../../assets/VSCode.png";
+import npm from "../../../assets/npm.png";
+import Vite from "../../../assets/Vite.png";
+import AWS from "../../../assets/AWS.png";
+import Vitest from "../../../assets/Vitest.png";
+import LeetCode from "../../../assets/LeetCode.png";
+import CodeWars from "../../../assets/CodeWars.png";
+
 export const technologies: string[] = [
-  "Java",
-  "HTML",
-  "CSS",
-  "Tailwind",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Node",
-  "Express",
-  "Postgres",
+  Java,
+  HTML,
+  CSS,
+  Tailwind,
+  JavaScript,
+  TypeScript,
+  React,
+  Node,
+  Express,
+  Postgres,
 ];
 
 export const tools: string[] = [
-  "Figma",
-  "Git",
-  "Vercel",
-  "Postman",
-  "VSCode",
-  "npm",
-  "Vite",
-  "AWS",
+  Figma,
+  Git,
+  Vercel,
+  Postman,
+  VSCode,
+  npm,
+  Vite,
+  AWS,
 ];
 
-export const learning: string[] = ["Vitest", "LeetCode", "CodeWars"];
+export const learning: string[] = [Vitest, LeetCode, CodeWars];
